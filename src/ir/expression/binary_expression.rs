4bf30aa1d28aac8fcfdfb33ad41a50fa2136ec3a
ShaneEverittM <shaneeverittmurphy@gmail.com>
2021-03-25 23:50:08 -0700

    Improve binary expression printing.

--- src/ir/expression/binary_expression.rs
+++ src/ir/expression/binary_expression.rs
@@ -23,7 +23,7 @@ impl BinaryExpression {
 impl IRNode for BinaryExpression {
     fn dump(&self, indent: u32) -> String {
         let indent_str = crate::util::make_indent(indent);
-        let mut output = format!("{}{}\n", indent_str, self.op);
+        let mut output = format!("{}BinaryExpression: {}\n", indent_str, self.op);
         output += &self.lhs.dump(indent + 1);
         output += &self.rhs.dump(indent + 1);
         output
